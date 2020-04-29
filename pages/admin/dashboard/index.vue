fc10ee1234dcb3d52d6f60a37618e1b8309d07d1
Alexsander <alexsander11115@gmail.com>
2020-04-29 10:15:11 +0300

    first commit

---
+++ pages/admin/dashboard/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-if="$store.state.auth">
+      <p>
+        You are authenticated. You can see the
+        <NuxtLink to="/admin/order">
+          order page
+        </NuxtLink>!
+        <NuxtLink to="/admin/profile">
+          profile page
+        </NuxtLink>!
+      </p>
+      <button @click="logout">
+        Logout
+      </button>
+    </div>
+    <p v-else>
+      Please
+      <NuxtLink to="/login">
+        login
+      </NuxtLink>.
+    </p>
+  </div>
+</template>
+
+<script>
+    const Cookie = process.client ? require('js-cookie') : undefined
+
+    export default {
+      name: "index",
+      methods: {
+        logout () {
+          Cookie.remove('auth')
+          this.$store.commit('setAuth', null)
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/admin/order/_id.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Id_value: {{ id }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "_id.vue",
+        data () {
+          return { id: this.validate() }
+        },
+        validate({params}) {
+          // Должен быть числом
+          return /^\d+$/.test(params.id)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
